26d5a6198d682910900bc1f3f98a807941349858
Fan Gao <gaofan83@hotmail.com>
2022-09-03 00:18:15 -0700

    Update run_ucsc_hic.sh

--- hic/run_ucsc_hic.sh
+++ hic/run_ucsc_hic.sh
@@ -31,3 +31,5 @@ awk -F "\t" '{print 0, $1, $2, 0, 0, $4, $5, 1, $7}' matrix_cool/WT2_DN2_gintera
 sort -k2,2d -k6,6d matrix_cool/WT2_DN2_ginteractions.tsv.short > matrix_cool/WT2_DN2_ginteractions.tsv.short.sorted
 ~/software/juicer-1.6/scripts/common/juicer_tools pre -r 10000,20000,50000,100000,250000,500000,1000000 \
  matrix_cool/WT2_DN2_ginteractions.tsv.short.sorted WT2_DN2.hic chrom_mm10.sizes
+
+#track type=hic name="Bcl11bKO1" bigDataUrl=http://bioinformatics.caltech.edu/events/Ellen/Bcl11bKO1_DN2.hic
